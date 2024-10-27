b66ec67dbb3107a922ca9fed19318caece78a716
Minwoo Jung <wmw04087@gmail.com>
2024-10-27 19:12:31 +0900

    feat: #6 초기 세팅 (#7)
    * feat: BaseEntity 공통 필드 추가
    
    * feat: 공통 예외 처리 구현
    
    * refactor: BaseEntity 필드명 수정

---
+++ src/main/java/boardcafe/boardpractice/common/exception/ErrorCode.java
@@ -0,0 +1,34 @@
+package boardcafe.boardpractice.common.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+import static org.springframework.http.HttpStatus.*;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorCode {
+
+    INVALID_REQUEST(1000, BAD_REQUEST, "올바르지 않은 요청입니다."),
+    INVALID_IMAGE_FORMAT(1001, BAD_REQUEST, "지원하지 않는 이미지 형식입니다."),
+    EXCEED_IMAGE_CAPACITY(1002, BAD_REQUEST, "이미지 크기가 허용된 최대 용량을 초과했습니다."),
+
+    INVALID_AUTHORITY(2001, FORBIDDEN, "해당 리소스에 접근할 권한이 없습니다."),
+    INVALID_PASSWORD(2002, UNAUTHORIZED, "비밀번호가 일치하지 않습니다."),
+    INVALID_ACCESS_TOKEN(2003, UNAUTHORIZED, "유효하지 않은 AccessToken 입니다."),
+    INVALID_REFRESH_TOKEN(2004, UNAUTHORIZED, "유효하지 않은 RefreshToken 입니다."),
+    EXPIRED_ACCESS_TOKEN(2005, UNAUTHORIZED, "만료된 AccessToken 입니다."),
+    EXPIRED_REFRESH_TOKEN(2006, UNAUTHORIZED, "만료된 RefreshToken 입니다."),
+
+    NOT_FOUND_BOARD_ID(3001, NOT_FOUND, "요청한 ID에 해당하는 게시물이 존재하지 않습니다."),
+    NOT_FOUND_USER_ID(3002, NOT_FOUND, "요청한 ID에 해당하는 사용자가 존재하지 않습니다."),
+    NOT_FOUND_USER_EMAIL(3003, NOT_FOUND, "요청한 이메일에 해당하는 사용자가 존재하지 않습니다."),
+    NOT_FOUND_IMAGE_FILE(3004, NOT_FOUND, "요청한 이미지 파일을 찾을 수 없습니다."),
+
+    SERVER_ERROR(4000, INTERNAL_SERVER_ERROR, "서버 에러가 발생하였습니다.");
+
+    private final int code;
+    private final HttpStatus httpStatus;
+    private final String message;
+}
---
+++ src/main/java/boardcafe/boardpractice/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,51 @@
+package boardcafe.boardpractice.common.exception;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static boardcafe.boardpractice.common.exception.ErrorCode.*;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            final MethodArgumentNotValidException e,
+            final HttpHeaders headers,
+            final HttpStatusCode status,
+            final WebRequest request
+    ) {
+        log.warn(e.getMessage(), e);
+        String errorMessage = e.getBindingResult().getAllErrors().getFirst().getDefaultMessage();
+        return ResponseEntity.badRequest()
+                .body(new ExceptionResponse(INVALID_REQUEST, errorMessage));
+    }
+
+    @ExceptionHandler(ConstraintViolationException.class)
+    public ResponseEntity<ExceptionResponse> handleConstraintViolationException(final ConstraintViolationException e) {
+        log.warn(e.getMessage(), e);
+        String errorMessage = e.getConstraintViolations().stream()
+                .map(ConstraintViolation::getMessage)
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("메시지 추출 도중 에러 발생"));
+        return ResponseEntity.badRequest()
+                .body(new ExceptionResponse(INVALID_REQUEST, errorMessage));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ExceptionResponse> handleException(final Exception e) {
+        log.error(e.getMessage(), e);
+        return ResponseEntity.internalServerError()
+                .body(new ExceptionResponse(SERVER_ERROR));
+    }
+}
